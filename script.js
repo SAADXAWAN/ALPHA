71b9508f0b78bce2932c80b9c2c39efdf8e384a5
SAADXAWAN <154013441+SAADXAWAN@users.noreply.github.com>
2023-12-16 12:46:25 -0800

    SCRIPT.JS
    JS DONE

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+function attemptLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorMessage = document.getElementById("error-message");
+
+    // Simple validation for demo purposes
+    if (username === "ALPHA" && password === "ALPHA") {
+        // Redirect to a dashboard or perform other actions upon successful login
+   
+        errorMessage.textContent = "";
+        window.location.href = "dashboard.htm"; // Redirect to a dashboard page
+    } else {
+        errorMessage.textContent = "Invalid username or password";
+    }
+}
